53d2ad6728f4cb837fcc0faca0519eed223b4bc3
Jorge Rivera <jorge.rivera@one.org>
2024-09-27 14:27:04 +0200

    pre post covid

--- stories/health_oda/trends.py
+++ stories/health_oda/trends.py
@@ -72,6 +72,51 @@ def health_share_trend(
     return data
 
 
+def pre_post_covid_trend(
+    prices: str = "constant",
+    base_year: int = 2022,
+) -> pd.DataFrame:
+
+    # Define the grouper
+    grouper = ["sector", "indicator"]
+
+    # Get the data for health
+    health_pre = (
+        get_bilateral_health_oda(
+            start_year=2017, end_year=2019, prices=prices, base_year=base_year
+        )
+        .assign(sector="Health", indicator="Pre-COVID")
+        .groupby(["year"] + grouper, dropna=False, observed=True)["value"]
+        .sum()
+        .reset_index()
+        .groupby(grouper, dropna=False, observed=True)["value"]
+        .mean()
+        .reset_index()
+    )
+
+    # Get the data for health
+    health_post = (
+        get_bilateral_health_oda(
+            start_year=2020, end_year=2022, prices=prices, base_year=base_year
+        )
+        .assign(sector="Health", indicator="Post-COVID")
+        .groupby(["year"] + grouper, dropna=False, observed=True)["value"]
+        .sum()
+        .reset_index()
+        .groupby(grouper, dropna=False, observed=True)["value"]
+        .mean()
+        .reset_index()
+    )
+    # Merge the data
+    data = (
+        pd.concat([health_pre, health_post], ignore_index=True)
+        .pivot(index="sector", columns="indicator", values="value")
+        .reset_index()
+    )
+
+    return data
+
+
 if __name__ == "__main__":
     lic_africa = low_income_and_africa_trend(1990, 2022)
     lic_africa.pivot(
@@ -86,3 +131,6 @@ if __name__ == "__main__":
             "share": "Health Share",
         }
     ).to_csv(config.Paths.health_oda / "health_share_trend.csv", index=False)
+
+    covid = pre_post_covid_trend()
+    covid.to_csv(config.Paths.health_oda / "pre_post_covid_trend.csv", index=False)
